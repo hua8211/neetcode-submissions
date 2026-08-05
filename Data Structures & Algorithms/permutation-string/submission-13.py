9f5a5172f343b73d5697f6f578582ec8a1b122e2
Hua Jiang <69730095+hua8211@users.noreply.github.com>
2026-08-04 21:02:04 -0400

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        temp1 = [0]*26
+        temp2 = [0]*26
+
+        for c in s1:
+            temp1[ord(c) - ord("a")] += 1
+
+        l,r = 0,0
+        while r <= len(s2)-1:
+            temp2[ord(s2[r]) - ord("a")] +=1
+            r += 1
+            if (r-l) == len(s1):
+                if temp1 == temp2:
+                    return True
+                else:
+                    temp2[ord(s2[l]) - ord("a")] -=1
+                    l += 1
+            
+            print(temp2)
+            print(r)
+            print(r-l)
+        
+        return False
