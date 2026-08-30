d5634b39d49cd7c1a4ce42ddf241f481819e9de8
Hua Jiang <69730095+hua8211@users.noreply.github.com>
2026-08-30 13:23:56 -0400

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        myMap = defaultdict(int)
+
+        for task in tasks:
+            myMap[task] += 1
+
+        maxHeap = []
+
+        for value in myMap.values():
+            maxHeap.append(-value)
+
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()
+
+        while maxHeap or q:
+            time += 1
+
+            # if not maxHeap:
+            #     time = q[0][1]
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
