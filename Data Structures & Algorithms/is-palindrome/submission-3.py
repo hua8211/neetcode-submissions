998ea669dafa5d8bb6fede2d217d54a4c3869dde
Hua Jiang <69730095+hua8211@users.noreply.github.com>
2026-09-13 13:03:18 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l, r = 0, len(s)-1
+
+        while l < r:
+            while s[l].isalnum() == False and l < r:
+                l += 1
+            while s[r].isalnum() == False and l < r:
+                r -= 1
+            
+            if ord(s[l]) != ord(s[r]):
+                return False
+            l += 1
+            r -= 1
+
+        return True
