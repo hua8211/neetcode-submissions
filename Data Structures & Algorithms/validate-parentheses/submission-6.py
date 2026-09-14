5fb6a3e4a0e14a64330cc2496f75c3d3dd4d29b4
Hua Jiang <69730095+hua8211@users.noreply.github.com>
2026-09-14 16:31:36 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        myMap = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        stack = []
+
+        for c in s:
+            if c in myMap:
+                if not stack:
+                    return False
+                elif stack.pop() != myMap[c]:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if len(stack) == 0 else False
